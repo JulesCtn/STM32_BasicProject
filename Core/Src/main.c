339a43d0699025866072aaa08e150f71b2eb8e7b
JulesCtn <jules.contadin@laposte.net>
2026-01-19 16:07:15 +0100

    USART3
    USART3 communication with PC

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -85,6 +85,10 @@ int main(void)
 	  // Toggle LED pin
 	  HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
 
+	  // Send '#' only once
+	  while (!(USART3->SR & USART_SR_TC));
+	  	  USART3->DR = '#';
+
 	  // Wait using a counting delay
 	  HAL_Delay(500);
   }
@@ -216,6 +220,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
